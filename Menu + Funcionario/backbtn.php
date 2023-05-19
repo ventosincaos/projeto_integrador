7bc01bfcd9fc4d27b82260e59ca5bb88ba1054bc
Nemo <129614934+HeyNautilus@users.noreply.github.com>
2023-05-19 13:42:41 -0300

    Add files via upload

---
+++ Menu + Funcionario/backbtn.php
@@ -0,0 +1,3 @@
+<div id="back-link-container">
+<a href="table.php" id="back-link"><ion-icon name="arrow-back-outline"></ion-icon></a>
+</div>
---
+++ Menu + Funcionario/create.php
@@ -0,0 +1,10 @@
+<?php
+ include_once("header.php");
+?>
+
+<?php include_once("backbtn.php"); ?>
+<h1>Aqui a Mari coloca o cadastro de funcionário</h1>
+
+<?php
+ include_once("footer.php");
+?>
