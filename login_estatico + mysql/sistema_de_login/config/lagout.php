250df5d36d78b766bc42c8c4332359715ce7f590
Nemo <129614934+HeyNautilus@users.noreply.github.com>
2023-06-17 14:16:22 -0300

    Delete login_estatico + mysql directory

--- login_estatico + mysql/sistema_de_login/config/lagout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['login']);
-unset($_SESSION['usuario']);
-
-header('location:index.php');
-
-?>
--- login_estatico + mysql/sistema_de_login/paginas/inicio.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include_once('head.php');
-?>
-
-<h1> Login correto! más página em manutenção </h1>
-
-<?php 
-include_once('footer.php');
-?>
