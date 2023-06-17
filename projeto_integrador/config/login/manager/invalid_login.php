d79524c519fe98fbf00474ad7c90a1761746d08c
Nemo <129614934+HeyNautilus@users.noreply.github.com>
2023-06-17 14:12:45 -0300

    Delete projeto_integrador directory

--- projeto_integrador/config/login/manager/invalid_login.php
+++
@@ -1,7 +0,0 @@
-<?php if(isset($_GET['loginError'])){ ?>
-
-<div class="alert alert-danger" role="alert">
-  <p class='font-weight-bold'> Credenciais invalidas. Por favor, tente novamente </p>
-</div>
-
-<?php } ?>
