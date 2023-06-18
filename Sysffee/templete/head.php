c0768deb801ca7b549ac83efee56e5e7f8699466
Pedro Henrique <104384284+Avariax@users.noreply.github.com>
2023-06-17 22:01:23 -0300

    Update head.php

--- Sysffee/templete/head.php
+++ Sysffee/templete/head.php
@@ -21,7 +21,7 @@ include_once("../config/url.php");
     <link rel="stylesheet" href="<?= $BASE_URL?>/../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?= $BASE_URL?>/../css/style1.css">
-   
+    <link rel="stylesheet" href="<?= $BASE_URL?>/../css/styleNav.css">
 
     <title>Document</title>
 </head>
