2b464b136fcba849f637e6a3db98324c673dff39
Pedro Henrique <104384284+Avariax@users.noreply.github.com>
2023-06-17 22:01:27 -0300

    Update header.php

--- Sysffee/templete/header.php
+++ Sysffee/templete/header.php
@@ -1,6 +1,3 @@
-
-
-
 <header>
 
     <div class="flex">
@@ -11,6 +8,20 @@
             <a href="tableProduct.php">Estoques</a>
             <a href="vendas.php">Vendas</a>
         </nav>
+        <div id="myNav" class="overlay">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <div class="overlay-right">
+            <div class="form">
+                <h2>Finalizar compra</h2>
+                <form action="/action_page.php">
+                    <label for="fname">Preço total:</label><br>
+                    <input type="text" id="fname" name="fname" value="R$"><br>
+                    <button type="button" value="Submit" class="Finalizar">Finalizar</button>
+                </form>
+                </div>
+            </div>
+            <div class="overlay-left"><!-- div Responsável pela parte ao lado de Finalizar compra --></div>
+        </div>
     </div>
 
 </header>
